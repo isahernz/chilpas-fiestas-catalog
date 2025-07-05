9af11c955b279df6c0959a6ecb90caaf4a28b1e0
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-05 17:57:55 -0600

    feat(data): add mock data for development

---
+++ app/data/celebrations.ts
@@ -0,0 +1,44 @@
+import { Celebration } from "@/app/types/celebration";
+
+export const celebrations: Celebration[] = [
+  {
+    id: "265532f2-ef87-49ee-b2e7-a2c9bc6ba68f",
+    name: "Graduación",
+  },
+  {
+    id: "2f99e5b7-5838-411f-af2c-748ca4489603",
+    name: "Fiesta de 15 años",
+  },
+  {
+    id: "36116ae9-2e9e-46b2-adf9-f6b1a92af2fe",
+    name: "Revelación de Género",
+  },
+  {
+    id: "61851765-1c3b-4227-b8ee-233e52f8365a",
+    name: "Primera Comunión",
+  },
+  {
+    id: "645fe4ef-67bb-49db-bff2-cc1d2fe4376a",
+    name: "Navidad",
+  },
+  {
+    id: "75bc1a1b-063d-46c9-a431-ebb6c070bd3b",
+    name: "Cumpleaños",
+  },
+  {
+    id: "863fb06a-263e-4138-b35d-04ff95ee8230",
+    name: "Bautizo",
+  },
+  {
+    id: "9ea5e443-f8cc-48e7-b1e9-1b6d292dc7f5",
+    name: "Despedida de Soltera/o",
+  },
+  {
+    id: "a1ccd12d-679c-4644-816a-d0d2fce72c4d",
+    name: "Aniversario",
+  },
+  {
+    id: "b5ce796c-9cb0-4af9-917e-da548e456272",
+    name: "Baby Shower",
+  },
+];
