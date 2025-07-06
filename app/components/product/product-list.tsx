57f4830084bff3ab65e02537147cac2238fe3efb
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-06 15:39:40 -0600

    fix(product-list): handle empty product list with user-friendly message

--- app/components/product/product-list.tsx
+++ app/components/product/product-list.tsx
@@ -4,6 +4,14 @@ import { getSearchProducts } from "@/app/supabase";
 export async function ProductList({ query }: { query: string }) {
   const products = await getSearchProducts(query);
 
+  if (products?.length === 0 || !products) {
+    return (
+      <section className="flex h-64 items-center justify-center">
+        <p className="text-gray-500">Ups! No se encontraron productos 😞</p>
+      </section>
+    );
+  }
+
   return (
     <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       {products?.map(product => (
