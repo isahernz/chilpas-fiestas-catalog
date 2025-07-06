6a99adad66ebbdada945ea7665522aabb4841532
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-06 13:53:54 -0600

    feat(product-page): add metadata generation for SEO

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,19 +1,38 @@
 import ProductDetails from "@/app/components/product/product-details";
 import ProductDetailsSkeleton from "@/app/components/ui/skeletons/product-details-skeleton";
+import { getCachedProductById } from "@/app/lib/data";
 import { Suspense } from "react";
 
 interface ProductPageProps {
   params: Promise<{ id: string }>;
 }
 
+export const generateMetadata = async ({ params }: ProductPageProps) => {
+  const product = await getCachedProductById((await params).id);
+  return {
+    title: `Chilpas Fiestas | ${product?.name} 🥳`,
+    description: product?.description,
+    openGraph: {
+      title: product?.name,
+      description: product?.description,
+      images: [
+        {
+          url: product?.cover,
+          width: 1200,
+          height: 630,
+          alt: product?.name,
+        },
+      ],
+    },
+  };
+};
+
 export default async function ProductPage({ params }: ProductPageProps) {
   const { id } = await params;
   return (
-    <section className="mx-auto max-w-4xl">
-      <Suspense fallback={<ProductDetailsSkeleton />}>
-        <ProductDetails id={id} />
-      </Suspense>
-    </section>
+    <Suspense fallback={<ProductDetailsSkeleton />}>
+      <ProductDetails id={id} />
+    </Suspense>
   );
 }
 
