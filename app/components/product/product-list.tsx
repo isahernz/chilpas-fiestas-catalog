2f10b0df8936d4c6160e5319354ba95269607cc4
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-05 18:05:14 -0600

    feat(ui): implement product card and list components

---
+++ app/components/product/product-list.tsx
@@ -0,0 +1,14 @@
+import { ProductCard } from "@/app/components/product/product-card";
+import { getAllProducts } from "@/app/services/productService";
+
+export async function ProductList() {
+  const products = await getAllProducts();
+
+  return (
+    <section className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+      {products?.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </section>
+  );
+}
