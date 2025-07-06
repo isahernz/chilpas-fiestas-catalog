c90a0495c29ff119c1fc50c04636ba39859ebdf3
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-06 13:53:07 -0600

    fix(product-list-skeleton): adjust grid column classes

--- app/components/ui/skeletons/product-list-skeleton.tsx
+++ app/components/ui/skeletons/product-list-skeleton.tsx
@@ -2,7 +2,7 @@ import ProductCardSkeleton from "@/app/components/ui/skeletons/product-card-skel
 
 const ProductListSkeleton = () => {
   return (
-    <section className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       {Array.from({ length: 6 }).map((_, i) => (
         <ProductCardSkeleton key={i} />
       ))}
