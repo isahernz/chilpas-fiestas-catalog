1c3522a134a9bbff31e15e3f226591efffb4d785
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-05 20:10:47 -0600

    feat(service): add mock data handling service

---
+++ app/services/product-service.ts
@@ -0,0 +1,28 @@
+import { Product } from "@/app/types/product";
+import { Celebration } from "@/app/types/celebration";
+import { products } from "@/app/data/products";
+import { celebrations } from "@/app/data/celebrations";
+
+export async function getAllProducts(): Promise<(Product & { celebration: Celebration })[]> {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return products.map(product => ({
+    ...product,
+    celebration: celebrations.find(c => c.id === product.celebrationId)!,
+  }));
+}
+
+export async function getProductById(id: string): Promise<Product & { celebration: Celebration }> {
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  const product = products.find(product => product.id === id);
+
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  return {
+    ...product,
+    celebration: celebrations.find(c => c.id === product.celebrationId)!,
+  };
+}
