4580172f79c51213ff43523f64b67ff59d9e6e2f
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-23 10:53:00 -0600

    feat(products): enhance getSearchProducts to handle short queries and fetch all products

--- app/supabase/index.ts
+++ app/supabase/index.ts
@@ -3,6 +3,26 @@ import { createClient } from "@/app/supabase/client";
 const supabase = createClient();
 
 export const getSearchProducts = async (query: string) => {
+  const trimmedQuery = query.trim();
+
+  if (!trimmedQuery || trimmedQuery.length < 3) {
+    try {
+      const { data: products, error } = await supabase
+        .from("products")
+        .select(`id, name, character, cover, gallery, content, description, celebrations (id, name)`);
+
+      if (error) {
+        console.error("Error fetching all products:", error);
+        throw new Error(`Error fetching all products: ${error.message}`);
+      }
+
+      return products;
+    } catch (error) {
+      console.error("Error en getSearchProducts (query vacía/corta):", error);
+      return null;
+    }
+  }
+
   try {
     const { data, error } = await supabase.rpc("search_products_with_celebration", { query });
 
