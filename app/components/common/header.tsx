0d37bdd1c2f16f97aa0a5a0dcd27c486beb32344
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-27 16:23:12 -0600

    fix(header): correct typo of "Catálogo" in Header component

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
           <ChilpasLogo />
           <div className="flex flex-col items-center gap-y-1 sm:items-end">
             <h1 className="bg-gradient-to-r from-pink-600 via-blue-600 to-yellow-600 bg-clip-text text-center text-xl leading-none font-bold text-balance text-transparent sm:text-right sm:text-2xl">
-              Catalogo de Productos
+              Catálogo de Productos
             </h1>
             <span className="text-sm text-gray-500">Encuentra el producto perfecto para tu fiesta 🥳</span>
           </div>
