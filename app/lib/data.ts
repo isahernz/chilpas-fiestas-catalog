4ce8bc90e9ca9d191196a8665d8988e2a4beece3
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-27 16:12:32 -0600

    refactor(supabase): remove unused client and data handling code

--- app/lib/data.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from "react";
-import { getProductById } from "@/app/supabase";
-
-export const getCachedProductById = cache(async (id: string) => {
-  const res = await getProductById(id);
-  return res;
-});
