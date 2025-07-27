6aae42fdc02ebe1cb94aaa9cafbaed939085373d
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-27 16:20:39 -0600

    feat(home): simplify Home component by removing query handling and loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
 import { Header } from "@/app/components/common/header";
 import { ProductList } from "@/app/components/product/product-list";
-import ProductListSkeleton from "@/app/components/ui/skeletons/product-list-skeleton";
-import { Suspense } from "react";
 
-type PageProps = {
-  searchParams?: Promise<{
-    query?: string;
-  }>;
-};
-
-export default async function Home({ searchParams }: PageProps) {
-  const params = await searchParams;
-  const query = typeof params?.query === "string" ? params.query : "";
-
+export default async function Home() {
   return (
     <>
       <Header />
       <main className="mx-auto max-w-4xl p-4">
-        <Suspense fallback={<ProductListSkeleton />}>
-          <ProductList query={query} />
-        </Suspense>
+        <ProductList />
       </main>
     </>
   );
