1ab7102b1e61cee8ada1ee5a92f3c304a301b492
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-29 20:37:42 -0600

    feat(ui): enhance ProductList with celebration filtering and dynamic API URL

--- app/components/product/product-list.tsx
+++ app/components/product/product-list.tsx
@@ -6,18 +6,29 @@ import { useApi } from "@/app/hooks/useApi";
 import { Product } from "@/app/types/product";
 import { useSearchParams } from "next/navigation";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const ProductList = () => {
   const searchParams = useSearchParams();
+
+  const celebration = searchParams.get("celebration");
   const query = searchParams.get("query") || "";
 
-  const apiUrl = query ? `/api/products?query=${encodeURIComponent(query)}` : "/api/products";
+  let apiUrl = `${API_BASE_URL}/products`;
+
+  if (celebration) {
+    apiUrl = `${API_BASE_URL}/products/by-celebration/${encodeURIComponent(celebration)}`;
+  } else if (query) {
+    apiUrl = `${API_BASE_URL}/products?query=${encodeURIComponent(query)}`;
+  }
+
   const { data: products, isLoading, error } = useApi<Product[]>(apiUrl);
 
   if (isLoading) return <ProductListSkeleton />;
 
   if (error) return <p className="text-red-500">{error}</p>;
 
-  if (!products || products.length === 0) {
+  if (products?.length === 0 || products === null || products === undefined) {
     return <p className="text-gray-500">No se encontraron productos</p>;
   }
 
