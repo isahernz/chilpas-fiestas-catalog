3b249d97671e48d9e529f47c9658698570a94e3a
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-29 20:33:27 -0600

    refactor(ui): update ProductSearch to use custom useDebounce and improve layout

--- app/components/product/product-search.tsx
+++ app/components/product/product-search.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { Input } from "@/app/components/ui/input";
+import { useDebouncedCallback } from "@/app/hooks/useDebounce";
 import { Search as SearchIcon } from "lucide-react";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export const ProductSearch = ({ placeholder }: { placeholder: string }) => {
+export function ProductSearch({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -23,14 +23,14 @@ export const ProductSearch = ({ placeholder }: { placeholder: string }) => {
   }, 500);
 
   return (
-    <div className="relative w-full">
+    <div className="relative flex w-full">
       <label htmlFor="search" className="sr-only">
         Buscar
       </label>
       <SearchIcon className="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 transform text-gray-400" />
       <Input
         id="search"
-        className="h-10 pl-10 text-sm"
+        className="w-full pl-10 text-sm"
         type="search"
         placeholder={placeholder}
         onChange={e => handleSearch(e.target.value)}
@@ -38,4 +38,4 @@ export const ProductSearch = ({ placeholder }: { placeholder: string }) => {
       />
     </div>
   );
-};
+}
