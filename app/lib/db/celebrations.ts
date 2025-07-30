296300c1b01265a2332e0cea0ecdd3fc3447f8cd
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-29 20:29:58 -0600

    feat(supabase): add functions for fetching celebrations

---
+++ app/lib/db/celebrations.ts
@@ -0,0 +1,35 @@
+import { createClient } from "@/app/lib/supabase/server";
+
+export async function getAllCelebrations() {
+  const supabase = await createClient();
+
+  const { data: celebrations, error } = await supabase.from("celebrations").select(`id, name, slug`);
+
+  if (error) throw new Error(error.message);
+
+  return celebrations;
+}
+
+export async function getCelebrationById(id: string) {
+  const supabase = await createClient();
+
+  const { data: celebration, error } = await supabase
+    .from("celebrations")
+    .select(`id, name, slug`)
+    .eq("id", id)
+    .single();
+
+  if (error) throw new Error(error.message);
+
+  return celebration;
+}
+
+export async function getCelebrationIdBySlug(slug: string) {
+  const supabase = await createClient();
+
+  const { data: celebration, error } = await supabase.from("celebrations").select(`id`).eq("slug", slug).single();
+
+  if (error) throw new Error(error.message);
+
+  return celebration.id;
+}
