d153d6a189a5c3b16dae5035e8101827500926d6
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-29 20:41:46 -0600

    feat(api): add celebration filtering to products API route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,17 +1,25 @@
-import { getAllProducts, searchProducts } from "@/app/lib/db/products";
+import { getAllProducts, getProductsByCelebration, searchProducts } from "@/app/lib/db/products";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query");
+  const celebration = searchParams.get("celebration");
 
-  if (!query) {
+  if (!query && !celebration) {
     const products = await getAllProducts();
     if (!products) return NextResponse.json({ error: "Error al obtener  productos" }, { status: 500 });
     return NextResponse.json(products);
   }
 
-  const products = await searchProducts(query);
+  if (celebration && !query) {
+    const products = await getProductsByCelebration(celebration);
+    if (!products) return NextResponse.json({ error: "Error al buscar  productos" }, { status: 500 });
+    return NextResponse.json(products);
+  }
+
+  const products = await searchProducts(query!);
+
   if (!products) return NextResponse.json({ error: "Error al buscar  productos" }, { status: 500 });
   return NextResponse.json(products);
 }
