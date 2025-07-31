f74f0ccc8d63089460f802289c11c076a6449d0c
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-30 21:42:10 -0600

    update logic endpoints

--- app/api/celebrations/route.ts
+++ app/api/celebrations/route.ts
@@ -1,10 +1,8 @@
 import { getAllCelebrations } from "@/app/lib/db/celebrations";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const celebrations = await getAllCelebrations();
-  if (!celebrations) return new Response("Error al obtener celebraciones", { status: 500 });
-  return new Response(JSON.stringify(celebrations), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  if (!celebrations) return NextResponse.json({ error: "Error al obtener celebraciones" }, { status: 500 });
+  return NextResponse.json(celebrations);
 }
--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,19 +1,17 @@
 import { getProductById } from "@/app/lib/db/products";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
 
   if (!id) {
-    return new Response("ID del producto es requerido", { status: 400 });
+    return NextResponse.json({ error: "ID del producto es requerido" }, { status: 400 });
   }
 
   const product = await getProductById(id);
 
   if (!product) {
-    return new Response("Producto no encontrado", { status: 404 });
+    return NextResponse.json({ error: "Producto no encontrado" }, { status: 404 });
   }
-  return new Response(JSON.stringify(product), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  return NextResponse.json(product);
 }
