d4e8790694c86b352763fb74db13ee3d073d3278
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-31 10:46:42 -0600

    update(ui): convert the filter panel in to a server component

--- app/components/filters/filter-panel.tsx
+++ app/components/filters/filter-panel.tsx
@@ -1,5 +1,12 @@
 import { CelebrationFilter } from "@/app/components/filters/celebration/celebration-filter";
 
-export function FilterPanel() {
-  return <CelebrationFilter />;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export async function FilterPanel() {
+  const data = await fetch(`${API_BASE_URL}/api/celebrations`, {
+    cache: "no-store",
+  });
+  const celebrations = await data.json();
+
+  return <CelebrationFilter celebrations={celebrations} />;
 }
