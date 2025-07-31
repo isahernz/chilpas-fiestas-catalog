34ae7c7244b02adfc33d002ae046114084d74ee2
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-07-30 20:28:06 -0600

    refactor url endpoints

--- app/components/product/product-info-details.tsx
+++ app/components/product/product-info-details.tsx
@@ -1,10 +1,10 @@
 import { Badge } from "@/app/components/ui/badge";
 import { Product } from "@/app/types";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || `https://${process.env.VERCEL_URL}/`;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const ProductInfoDetails = async ({ id }: { id: string }) => {
-  const res = await fetch(`${API_BASE_URL}api/products/${id}`);
+  const res = await fetch(`${API_BASE_URL}/api/products/${id}`);
   const product = (await res.json()) as Product;
 
   return (
