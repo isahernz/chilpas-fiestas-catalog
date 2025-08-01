0599fa617a9b8d4118e7ee7b36224ced379ef2f0
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-08-01 08:15:26 -0600

    feat(ui): add link to catalog in products section

--- components/sections/ProductsGrid.tsx
+++ components/sections/ProductsGrid.tsx
@@ -1,6 +1,8 @@
 import { ProductCard } from "@/app/catalogo/components/products";
 import { Product } from "@/app/types";
 import { Badge } from "@/components/ui/badge";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -9,7 +11,7 @@ export async function ProductsGrid() {
   const products: Product[] = await res.json();
 
   return (
-    <section id="catalogo" className="rounded-3xl bg-gradient-to-br from-gray-50 to-white px-4 py-10 lg:p-16">
+    <section id="catalogo" className="space-y-8 rounded-3xl bg-gradient-to-br from-gray-50 to-white px-4 py-10 lg:p-16">
       <div className="container mx-auto px-4">
         <div className="mb-16 space-y-4 text-center">
           <Badge className="rounded-full border border-purple-200 bg-gradient-to-r from-purple-100 to-pink-100 px-4 py-2 text-sm font-medium text-purple-700">
@@ -34,6 +36,14 @@ export async function ProductsGrid() {
           ))}
         </div>
       </div>
+
+      <Link
+        href="/catalogo"
+        className="group mx-auto flex max-w-max gap-x-2 rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 px-4 py-2 text-sm font-medium text-white shadow-lg hover:from-purple-700 hover:to-pink-700"
+      >
+        <span>Ir al catálogo digital</span>
+        <ArrowRight className="size-5 transition-transform group-hover:translate-x-1" />
+      </Link>
     </section>
   );
 }
