96bdedd7ee7e29d5ff8423fa7bece04166486c11
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-08-01 00:09:18 -0600

    feat(ui): logo redirect to index page

--- app/catalogo/components/NavbarCatalog.tsx
+++ app/catalogo/components/NavbarCatalog.tsx
@@ -1,13 +1,16 @@
 import { FilterPanel } from "@/app/catalogo/components/filters";
 import { SearchBar } from "@/app/catalogo/components/SearchBar";
 import { ChilpasLogo } from "@/components/common/ChilpasLogo";
+import Link from "next/link";
 
 export function NavbarCatalog() {
   return (
     <div className="bg-white shadow-sm">
       <header className="mx-auto flex w-full max-w-4xl flex-col items-center justify-center gap-y-3 p-4">
         <div className="flex w-full flex-col items-center gap-y-3 sm:flex-row sm:justify-between sm:gap-y-0">
-          <ChilpasLogo />
+          <Link href="/">
+            <ChilpasLogo />
+          </Link>
           <div className="flex flex-col items-center gap-y-1 sm:items-end">
             <h1 className="bg-gradient-to-r from-pink-600 via-blue-600 to-yellow-600 bg-clip-text text-center text-xl leading-none font-bold text-balance text-transparent sm:text-right sm:text-2xl">
               Catálogo de Productos
