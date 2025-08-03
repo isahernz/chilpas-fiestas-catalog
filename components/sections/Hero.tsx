12e12a25855e26fc96174a3ab62fc8c78767892e
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-08-03 15:40:56 -0600

    feat(ui): enhance Hero section layout and styling for improved user experience

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,44 +1,46 @@
 import { ArrowRight } from "lucide-react";
 import { Badge } from "../ui/badge";
 import Link from "next/link";
+import { HighlightedText } from "@/components/shared/HighlightedText";
 
 export function Hero() {
   return (
-    <section id="inicio" className="w-full space-y-10 text-center">
-      <Badge variant="landing">¡Hacemos realidad tu fiesta ideal! 🥳</Badge>
-      <div className="space-y-4">
-        <h1 className="text-4xl font-bold text-gray-900 lg:text-7xl">
-          Celebraciones que
-          <span className="block bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
-            inspiran y emocionan
-          </span>
+    <section
+      id="inicio"
+      className="w-full space-y-10 text-center lg:flex lg:items-center lg:justify-between lg:space-y-0 lg:gap-x-5"
+    >
+      <div className="flex flex-col items-center gap-y-5 md:mx-auto md:my-10 md:max-w-2xl lg:mx-0 lg:w-2/3 lg:max-w-full lg:items-start">
+        <Badge variant="landing" className="animate-bounce">
+          ¡Hacemos realidad tu fiesta ideal! 🥳
+        </Badge>
+        <h1 className="text-4xl font-bold text-balance text-gray-900 md:text-[40px] lg:text-start xl:text-[54px]">
+          Celebraciones que <HighlightedText>inspiran</HighlightedText> y <HighlightedText>emocionan</HighlightedText>
         </h1>
-        <p className="mx-auto max-w-2xl text-base leading-relaxed text-balance text-gray-600">
+        <p className="text-center text-sm leading-relaxed text-balance text-gray-600 md:text-base lg:text-start xl:text-lg">
           En Chilpas Fiestas tenemos todo lo que necesitas para hacer de tu celebración un éxito. Desde decoraciones
-          personalizadas hasta productos únicos, ¡podemos ayudarte a encontrar lo que buscas
+          personalizadas hasta productos únicos. ¡Podemos ayudarte a encontrar lo que buscas!
         </p>
         <Link
           href="/catalogo"
-          className="group mx-auto flex max-w-max gap-x-2 rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 px-4 py-2 text-sm font-medium text-white shadow-lg hover:from-purple-700 hover:to-pink-700"
+          className="group flex max-w-max gap-x-2 rounded-full bg-gradient-to-r from-purple-600 to-pink-600 px-4 py-2 shadow-lg hover:from-purple-700 hover:to-pink-700"
         >
-          <span>Explora nuestros productos en nuestro catálogo digital</span>
-          <ArrowRight className="size-5 transition-transform group-hover:translate-x-1" />
+          <span className="text-center text-sm font-medium text-white">Explora nuestro catálogo digital</span>
+          <ArrowRight className="size-5 stroke-white transition-transform group-hover:translate-x-1" />
         </Link>
       </div>
-      <div className="relative">
-        <div className="aspect-video overflow-hidden rounded-3xl bg-gray-100 shadow-2xl">
-          <video className="h-full w-full object-cover" autoPlay muted loop playsInline>
-            <source
-              src="https://res.cloudinary.com/dgmsevscb/video/upload/v1754011866/intro_chilpas_kubbqm.mp4"
-              type="video/mp4"
-            />
-            Tu navegador no soporta videos HTML5.
-          </video>
 
-          <Badge className="absolute top-3 right-3 rounded-full border border-purple-200 bg-gradient-to-r from-purple-100 to-pink-100 text-xs text-purple-700">
-            ¡Todo para tus fiestas! 🎉
-          </Badge>
-        </div>
+      <div className="relative aspect-video overflow-hidden rounded-3xl bg-gray-100 shadow-2xl">
+        <video className="h-full w-full object-cover" autoPlay muted loop playsInline>
+          <source
+            src="https://res.cloudinary.com/dgmsevscb/video/upload/v1754011866/intro_chilpas_kubbqm.mp4"
+            type="video/mp4"
+          />
+          Tu navegador no soporta videos HTML5.
+        </video>
+
+        <Badge variant="landing" className="absolute top-3 right-3 px-2 py-0.5 text-xs text-purple-700">
+          ¡Todo para tus fiestas! 🎉
+        </Badge>
       </div>
     </section>
   );
