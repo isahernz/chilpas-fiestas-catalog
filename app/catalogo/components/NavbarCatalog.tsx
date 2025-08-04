e47ab1169bc534c78529a8754be4fb236318743f
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-08-03 19:35:47 -0600

    refactor(ui): simplify NavbarCatalog component layout and remove unused elements

--- app/catalogo/components/NavbarCatalog.tsx
+++ app/catalogo/components/NavbarCatalog.tsx
@@ -1,28 +1,11 @@
 import { FilterPanel } from "@/app/catalogo/components/filters";
 import { SearchBar } from "@/app/catalogo/components/SearchBar";
-import { ChilpasLogo } from "@/components/common/ChilpasLogo";
-import Link from "next/link";
 
 export function NavbarCatalog() {
   return (
-    <div className="bg-white shadow-sm">
-      <header className="mx-auto flex w-full max-w-4xl flex-col items-center justify-center gap-y-3 p-4">
-        <div className="flex w-full flex-col items-center gap-y-3 sm:flex-row sm:justify-between sm:gap-y-0">
-          <Link href="/">
-            <ChilpasLogo />
-          </Link>
-          <div className="flex flex-col items-center gap-y-1 sm:items-end">
-            <h1 className="bg-gradient-to-r from-pink-600 via-blue-600 to-yellow-600 bg-clip-text text-center text-xl leading-none font-bold text-balance text-transparent sm:text-right sm:text-2xl">
-              Catálogo de Productos
-            </h1>
-            <span className="text-sm text-gray-500">Encuentra el producto perfecto para tu fiesta 🥳</span>
-          </div>
-        </div>
-        <div className="flex w-full items-center gap-x-3">
-          <SearchBar placeholder="¿Listo para armar tu fiesta?... 🎉🎈🥳" />
-          <FilterPanel />
-        </div>
-      </header>
+    <div className="mx-auto flex flex-col items-center gap-3 rounded-lg bg-white p-4 shadow-md sm:flex-row sm:justify-between">
+      <SearchBar placeholder="¿Listo para armar tu fiesta?... 🎉🎈🥳" />
+      <FilterPanel />
     </div>
   );
 }
