42fd690b92103d59093aef6a8658e77d2e09ff5f
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-08-03 18:57:04 -0600

    feat(ui): add navbar links interface and initial links array

---
+++ app/consts/navbar-links.ts
@@ -0,0 +1,13 @@
+interface NavLink {
+  href: string;
+  label: string;
+}
+
+export const links: NavLink[] = [
+  { href: "#conocenos", label: "Conócenos" },
+  { href: "#inicio", label: "Inicio" },
+  { href: "#celebraciones", label: "Celebraciones" },
+  { href: "#catalogo", label: "Cátalogo" },
+  { href: "/catalogo", label: "Buscador de productos" },
+  { href: "#contacto", label: "Contáctanos" },
+];
